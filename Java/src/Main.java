cb027e2938fa0ca4ec4e0de31c86d269bb741e46
Jorgio23 <jorgeroa080@gmail.com>
2026-09-10 20:30:27 -0500

    Variables y tipos de datos

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nombre = "Andres";
+        int edad = 25; // pon tu edad real
+        double estatura = 1.75; // tu estatura en metros
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Estatura: " + estatura);
+        System.out.println("El próximo año tendré " + (edad + 1) + " años");
     }
 }
